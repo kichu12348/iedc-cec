0d790ca1f6cca1332b8e0c9bf3816653448af1e7
Mahadevan Reji <145147243+kichu12348@users.noreply.github.com>
2024-04-05 12:39:28 +0530

    Update three.jsx

--- src/components/3DsTUFF/three.jsx
+++ src/components/3DsTUFF/three.jsx
@@ -41,6 +41,7 @@ camera.rotation.y= 20;
     });
     const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
     scene.add(sphere);
+    sphere.rotation.x=20
 
     // Add lighting
     const ambientLight = new THREE.AmbientLight(0x404040);
